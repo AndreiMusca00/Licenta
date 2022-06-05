0daff6f7cbdddad80588154bbfee434976799a66
AndreiMusca00 <andrei.m150@Yahoo.com>
2022-06-05 19:31:56 +0300

    TryFailed

---
+++ BookingAPI/Migrations/20220605162247_AddEndDateReservare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingAPI.Migrations
+{
+    public partial class AddEndDateReservare : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataFinal",
+                table: "Rezervare",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataFinal",
+                table: "Rezervare");
+        }
+    }
+}
---
+++ BookingAPI/Migrations/20220605162943_CorrectDates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingAPI.Migrations
+{
+    public partial class CorrectDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataFinal",
+                table: "Rezervare");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataFinal",
+                table: "Rezervare",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
